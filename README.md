429c01d1abc4ffebae88e63995831936104915d5
Frank Villaro-Dixon <frank@vi-di.fr>
2016-12-08 17:15:56 +0100

    pypi conf + README + licence

---
+++ README.md
@@ -0,0 +1,7 @@
+# elevationapi — Get the elevation of (almost) any point on earth
+python-elevationapi is a simple library that interfaces with 
+http://www.elevationapi.xyz .
+
+
+## How to use
+Usage is very simple:
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+	name = 'elevationapi',
+	packages = ['elevationapi'],
+	version = '0.1',
+	description = 'Get the elevation of any land point on Earth using elevationapi.xyz',
+	author = 'Frank Villaro-Dixon',
+	author_email = 'frank@villaro-dixon.eu',
+	url = 'https://github.com/Frankkkkk/python-elevationapi',
+	download_url = 'https://github.com/Frankkkkk/python-elevationapi/tarball/0.1',
+	keywords = 'elevation altitude earth elevationapi.xyz',
+	classifiers = [
+		'Topic :: Scientific/Engineering :: Atmospheric Science',
+		'Topic :: Scientific/Engineering :: GIS',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.2',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		],
+)
